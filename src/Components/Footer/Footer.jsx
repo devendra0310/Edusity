a6cf94f4853c6edf167702b2669b35cbafc21e8e
Devendra Singh <devendrasingh@Devendras-MacBook-Air.local>
2024-05-27 20:34:50 +0530

    done

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Made by 💚 with Devendra </p>
+      <ul>
+        <li><a href="/" target='_blank'>Terms of Services</a></li>
+        <li><a href="/" target='_blank'>Privacy Policy</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
